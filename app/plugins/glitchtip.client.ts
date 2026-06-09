a681a96f6b31382d3f92da20480659eddd3b772d
alexy103 <alexy103@hotmail.fr>
2026-06-09 11:08:23 +0200

    Update glitchtip dsn example link

--- app/plugins/glitchtip.client.ts
+++ app/plugins/glitchtip.client.ts
@@ -4,7 +4,7 @@ import { defineNuxtPlugin } from "nuxt/app";
 export default defineNuxtPlugin((nuxtApp) => {
   Sentry.init({
     app: nuxtApp.vueApp,
-    dsn: "http://5eac1a85ec6f4535b827f2e723dee9cb@localhost:8000/1",
+    dsn: "CHANGE_ME@localhost:8000/1",
     tracesSampleRate: 1,
     integrations: [Sentry.browserTracingIntegration()],
   });
